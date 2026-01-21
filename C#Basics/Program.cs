98cfe89abf015480e12741de5e84b66bcff00210
Merin999 <merinmathew191@gmail.com>
2026-01-21 13:24:43 +0400

    Initial C# basics and project setup

---
+++ C#Basics/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int age = 27;
+        double salary = 41000.50;
+        string name = "Meirn";
+        bool isEmployed = false;
+        char grade = 'A';
+
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Salary: " + salary);
+        Console.WriteLine("Employment Status: " + isEmployed);
+        Console.WriteLine("Grade: " + grade);
+    }
+
+}
